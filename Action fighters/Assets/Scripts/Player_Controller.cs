e3023e7250581d0db195341d7949fd17d215a0c2
ColeDunford <110067341+ColeDunford@users.noreply.github.com>
2023-06-08 14:29:55 -0700

    fixed animation transition

--- Action fighters/Assets/Scripts/Player_Controller.cs
+++ Action fighters/Assets/Scripts/Player_Controller.cs
@@ -7,10 +7,13 @@ public class Player_Controller : MonoBehaviour
     public float horizontalInput;
     public float speed = 10.0f;
     public float xRange = 10;
+    private Rigidbody2D rb;
+    public float jump;
+    
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
@@ -19,5 +22,10 @@ public class Player_Controller : MonoBehaviour
         //move left and right
         horizontalInput = Input.GetAxis("Horizontal");
         transform.Translate(Vector3.right * horizontalInput * Time.deltaTime * speed);
+        
+        if (Input.GetKeyDown(KeyCode.W))
+        {
+            rb.AddForce(new Vector2(rb.velocity.x, jump));
+        }
     }
 }
