7793e2568fda3eff9e42c8c83eae569ace39528a
ColeDunford <110067341+ColeDunford@users.noreply.github.com>
2023-06-09 10:49:34 -0700

    menu templates

--- Action fighters/Assets/Scripts/Player_Controller.cs
+++ Action fighters/Assets/Scripts/Player_Controller.cs
@@ -7,25 +7,35 @@ public class Player_Controller : MonoBehaviour
     public float horizontalInput;
     public float speed = 10.0f;
     public float xRange = 10;
-    private Rigidbody2D rb;
+    private Rigidbody2D PlayerRb;
+    private Animation PlayerAnimator;
     public float jump;
-    
+    public float movemetTest;
+    public AnimationClip RunClip, IdleClip;
+    bool canStart = true, canStop = true;
     // Start is called before the first frame update
     void Start()
     {
-        rb = GetComponent<Rigidbody2D>();
+        PlayerRb = GetComponent<Rigidbody2D>();
+        PlayerAnimator = GetComponent<Animation>();
     }
 
     // Update is called once per frame
     void Update()
     {
+        movemetTest = Input.GetAxisRaw("Horizontal");
         //move left and right
         horizontalInput = Input.GetAxis("Horizontal");
         transform.Translate(Vector3.right * horizontalInput * Time.deltaTime * speed);
-        
+
+        if ((Input.GetAxisRaw("Horizontal") > 0 || Input.GetAxisRaw("Horizontal") < 0) && canStart) { canStart = false; canStop = true; PlayerAnimator.Stop(); PlayerAnimator.clip = (RunClip); PlayerAnimator.Play(); }
+        else if (Input.GetAxisRaw("Horizontal") == 0 && canStart) { canStop = false; canStart = true; PlayerAnimator.Stop(); PlayerAnimator.clip = (IdleClip); PlayerAnimator.Play(); }
+
+       
         if (Input.GetKeyDown(KeyCode.W))
         {
-            rb.AddForce(new Vector2(rb.velocity.x, jump));
+            PlayerRb.AddForce(new Vector2(PlayerRb.velocity.x, jump));
         }
     }
+
 }
