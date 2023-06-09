d8c5171e89a64b35b9cd1659e05f4904f53ade6e
ColeDunford <110067341+ColeDunford@users.noreply.github.com>
2023-06-09 13:00:04 -0700

    My Way

--- Action fighters/Assets/Scripts/Player_Controller.cs
+++ Action fighters/Assets/Scripts/Player_Controller.cs
@@ -8,29 +8,26 @@ public class Player_Controller : MonoBehaviour
     public float speed = 10.0f;
     public float xRange = 10;
     private Rigidbody2D PlayerRb;
-    private Animation PlayerAnimator;
+    private Animator Anim;
     public float jump;
-    public float movemetTest;
-    public AnimationClip RunClip, IdleClip;
-    bool canStart = true, canStop = true;
+    
     // Start is called before the first frame update
     void Start()
     {
         PlayerRb = GetComponent<Rigidbody2D>();
-        PlayerAnimator = GetComponent<Animation>();
+        Anim = GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        movemetTest = Input.GetAxisRaw("Horizontal");
+        
         //move left and right
         horizontalInput = Input.GetAxis("Horizontal");
         transform.Translate(Vector3.right * horizontalInput * Time.deltaTime * speed);
-
-        if ((Input.GetAxisRaw("Horizontal") > 0 || Input.GetAxisRaw("Horizontal") < 0) && canStart) { canStart = false; canStop = true; PlayerAnimator.Stop(); PlayerAnimator.clip = (RunClip); PlayerAnimator.Play(); }
-        else if (Input.GetAxisRaw("Horizontal") == 0 && canStart) { canStop = false; canStart = true; PlayerAnimator.Stop(); PlayerAnimator.clip = (IdleClip); PlayerAnimator.Play(); }
-
+        if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.D))
+            Anim.SetBool("IsMoving", true);
+        else Anim.SetBool("IsMoving", false);
        
         if (Input.GetKeyDown(KeyCode.W))
         {
